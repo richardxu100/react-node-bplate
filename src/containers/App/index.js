081b4a9aaef18ee9c719aa2bd0a8c692aba5880d
Rich Xu <richardxu100@gmail.com>
2016-11-23 12:59:21 -0500

    Added mobx and jest testing

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,9 +4,18 @@ import logo from './logo.svg';
 import './style.css';
 
 export default class App extends Component {
-  // static propTypes = {}
-  // static defaultProps = {}
-  // state = {}
+  constructor() {
+    super();
+    this.state = { text: '' }
+  }
+
+  handleTextChange = (e) => this.setState({text: e.target.value});
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.commentStore.addComment(this.state.text);
+    this.setState({text: ''});
+  }
 
   render() {
     return (
@@ -18,6 +27,20 @@ export default class App extends Component {
         <p className="App-intro">
           To asdfasf started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.handleTextChange}
+            value={this.state.text} />
+          <input type="submit" style={{display: 'none'}}/>
+        </form>
+
+        <ul>
+          {this.props.commentStore.comments.map((comment, i) =>
+            <li key={i}>{comment.text}</li>
+          )}
+        </ul>
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Provider } from 'mobx-react';
 
 import App from './containers/App';
 import About from './containers/About';
 import NotFound from './containers/NotFound';
 import MainLayout from './containers/MainLayout';
+import { commentStore } from './stores/CommentStore';
 
 // we're creating a component!
 const routes = (
-  <MuiThemeProvider>
-    <Router history={browserHistory}>
-      <Route path="/" component={MainLayout}>
-        <IndexRoute component={App} />
-        <Route path="/about" component={About} />
-        <Route path="*" component={NotFound} />
-      </Route>
-    </Router>
-  </MuiThemeProvider>
+  <Provider commentStore={commentStore}>
+    <MuiThemeProvider>
+      <Router history={browserHistory}>
+        <Route path="/" component={MainLayout}>
+          <IndexRoute component={App} />
+          <Route path="/about" component={About} />
+          <Route path="*" component={NotFound} />
+        </Route>
+      </Router>
+    </MuiThemeProvider>
+  </Provider>
 )
 
 export default routes;
